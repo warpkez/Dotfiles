d8f73fac1e7032f1c1b5ad035af0789a805ea0d6
Wayne Jackson <warpkez@hotmail.com>
2023-09-03 10:09:53 +1000

    Updated sway install script

--- swayinstall.sh
+++ swayinstall.sh
@@ -2,11 +2,20 @@
 
 PKGS="sway swaybg wofi fonts-font-awesome clipman waybar swaylock kitty rofi thunar-font-manager alacritty dolphin"
 DIRS="sway waybar swaylock wofi kitty lxterminal nvim scripts"
+
+echo "Installing Sway and other packages" 
 sudo apt install -y $PKGS && touch .success
 
+echo "Copying config files"
 if [ -e .success ]; then
  for dir in $DIRS; do
     [ -d ~/.config/$dir ] || mkdir ~/.config/$dir
     cp $dir/* ~/.config/$dir
-     done
+ done
+ cd ~/.config/scripts && chmod +x * && cd
+ echo "Done"
+else
+ echo "Installation failed"    
 fi
+
+rm .success
